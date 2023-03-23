8b4307306b0be0fb816bc4ee1a0c9391582cc28c
Pete Hering <pjhering@gmail.com>
2023-03-23 09:02:56 -0400

    no longer using singleton pattern

--- src/Assets.hpp
+++ src/Assets.hpp
@@ -8,16 +8,12 @@ namespace pixel
   class Assets
   {
   public:
+    Assets ();
     ~Assets ();
-    static Assets *getInstance ();
     void loadTexture (const std::string *path);
     sf::Texture *getTexture (const std::string *path);
     
   private:
-    Assets *instance = nullptr;
-    Assets () {}
-    Assets (Assets const&) = delete;
-    Assets& operator= (Assets const&) = delete;
     std::unordered_map<std::string, *sf::Texture> textures ();
   };
 }
