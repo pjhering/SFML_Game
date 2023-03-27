6184760d42c3876f19d8f88db7b4cdce6eb14cb9
Pete Hering <pjhering@gmail.com>
2023-03-27 11:48:49 -0400

    add music and fonts

--- src/Assets.hpp
+++ src/Assets.hpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <unordered_map>
 #include <SFML/Audio.hpp>
+#include <SFML/Font.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Music.hpp>
 
 namespace pixel
 {
@@ -13,9 +15,13 @@ namespace pixel
         ~Assets ();
         sf::Texture *getTexture (std::string id);
         sf::Sound *getSound (std::string id);
+        sf::Music *getMusic (std::string id);
+        sf::Font *getFont (std::string id);
 
     private:
         std::unordered_map<std::string, sf::Texture *> textures;
         std::unordered_map<std::string, sf::Sound *> sounds;
+        std::unordered_map<std::string, sf::Music *> music;
+        std::unordered_map<std::string, sf::Font *> fonts;
     };
 }
